faf3065f342cc751cce616bbc04d6a7c3666c430
ericz02 <ezheng0720@gmail.com>
2023-04-17 23:43:11 -0400

    Adds header and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from "react";
-import Navigation from "./components/Navigation";
+import Navigation from "./components/navigation/Navigation";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+
         <Navigation />
+        <Home />
+        
+
+
+
+
+
         <Routes>{/* <Route path="/" element={} /> */}</Routes>
       </div>
     </BrowserRouter>
