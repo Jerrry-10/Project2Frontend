71b02cedbbb8e06c234c3448190b597567d240fc
ericz02 <ezheng0720@gmail.com>
2023-04-18 21:00:31 -0400

    Adds tailwind styling to Create Page

--- src/pages/CreateBlog.jsx
+++ src/pages/CreateBlog.jsx
@@ -17,23 +17,33 @@ const BlogCreate = () => {
 
   return (
     <div>
-      <h1>TTEST</h1>
-      <input
-        type="text"
-        placeholder="Enter post title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <br />
-      <textarea
-        placeholder="Enter post content"
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-        rows="10"
-        cols="50"
-      ></textarea>
-      <br />
-      <button onClick={handlePostSubmit}>Create Post</button>
+      <h1 className="flex justify-center mt-[2rem] text-[20px] text-neutral-700"><strong className="text-violet-700">Create</strong>Page</h1>
+      <div className="text-center rounded px-8 pt-6 pb-8 mb-[1rem]">
+        <input
+          className="shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+          placeholder="Enter post title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <br />
+        <textarea
+          className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none focus:shadow-outline mt-[40px] mb-[20px]"
+          placeholder="Enter post content"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          rows="10"
+          cols="50"
+        ></textarea>
+        <br />
+        <button 
+          className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 
+          border-b-4 border-blue-700 hover:border-blue-500 rounded" 
+          onClick={handlePostSubmit}>
+            Create Post
+        </button>
+
+      </div>
     </div>
   );
 };
