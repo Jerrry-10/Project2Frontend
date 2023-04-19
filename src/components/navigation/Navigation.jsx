3f483e153a8f09e21dfffa190ce042c38e3ed763
ericz02 <ezheng0720@gmail.com>
2023-04-18 22:00:34 -0400

    unadd about page

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -13,7 +13,6 @@ const Navigation = () => {
 
             <div className="mexu flex gap-8">
                 <li className="text-neutral-100 hover:text-violet-700"><Link to  = "/">Home</Link></li>
-                <li className="text-neutral-100 hover:text-violet-700"><Link to = "/about">About</Link></li>
                 <li className="text-neutral-100 hover:text-violet-700"><Link to = "/contact">Contact</Link></li>
                 <li className="text-neutral-100 hover:text-violet-700"><Link to = "/create">Create</Link></li>
             </div>
