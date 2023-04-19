0ca282d9f618034a00d704f9ce81b01621b7c114
Jerrry-10 <jerryaviles10@gmail.com>
2023-04-19 08:01:48 -0400

    fixes post hook

--- src/pages/CreateBlog.jsx
+++ src/pages/CreateBlog.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
-import createPost from "../hook/posthook";
+import useCreatePost from "../hook/posthook"; // Import the useCreatePost hook
 
 const BlogCreate = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
+  const { postData } = useCreatePost(); // Use the custom hook
+
   const handlePostSubmit = async () => {
     try {
-      await createPost({ title, content });
+      await postData(title, content); // Call postData function from the hook
       alert("Post created successfully!");
     } catch (error) {
       console.error("Error creating post:", error);
@@ -17,7 +19,9 @@ const BlogCreate = () => {
 
   return (
     <div>
-      <h1 className="flex justify-center mt-[2rem] text-[20px] text-neutral-700"><strong className="text-violet-700">Create</strong>Page</h1>
+      <h1 className="flex justify-center mt-[2rem] text-[20px] text-neutral-700">
+        <strong className="text-violet-700">Create</strong>Page
+      </h1>
       <div className="text-center rounded px-8 pt-6 pb-8 mb-[1rem]">
         <input
           className="shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -36,13 +40,13 @@ const BlogCreate = () => {
           cols="50"
         ></textarea>
         <br />
-        <button 
+        <button
           className="bg-violet-500 hover:bg-blue-400 text-white font-bold py-2 px-4 
-          border-b-4 border-violet-700 hover:border-blue-500 rounded" 
-          onClick={handlePostSubmit}>
-            Create Post
+          border-b-4 border-violet-700 hover:border-blue-500 rounded"
+          onClick={handlePostSubmit}
+        >
+          Create Post
         </button>
-
       </div>
     </div>
   );
